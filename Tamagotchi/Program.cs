58c56ae04970a3bc45749ebaa861a8703ebab98b
0zzysome <oscar.rosin@elev.ga.ntig.se>
2022-08-31 13:24:53 +0200

    tama

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Tamagotchi t1 = new Tamagotchi();
+
+
+Console.WriteLine("Name your Tamagotchi: ");
+t1.Name = Console.ReadLine();
+while(t1.GetAlive()==true)
+{
+    Console.Clear();
+    t1.PrintStats();
+    Console.WriteLine("Now what do you want to do?");
+    Console.WriteLine("1. Teach "+t1.Name);
+    Console.WriteLine("2. Talk with "+t1.Name);
+    Console.WriteLine("3. Feed "+t1.Name);
+    Console.WriteLine("4. Do nothing.");
+    string action = Console.ReadLine();
+
+    if(action == "1")
+    {
+
+    }
+
+
+
+}
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -2,5 +2,58 @@
 
 public class Tamagotchi
 {
-    
+    int Hunger = 0;
+    int Boredom =0;
+    List<string> Words = new List<string>();
+    bool isAlive = true;
+    Random generator;
+    public string Name;
+    public void Feed()
+    {
+        Hunger--;
+    }
+    private void ReduceBoredom()
+    {
+        Boredom--;
+    }
+    public void Teach(string Word)
+    {
+        Words.Add(Word);
+    }
+
+    public void hi()
+    {
+        int i = generator.Next(Words.Count);
+        Console.WriteLine(Words[i]);
+    }
+    public void PrintStats()
+    {
+        Console.WriteLine("Boredom: "+Boredom);
+        Console.WriteLine("Hunger: "+Hunger);
+        if(isAlive==true)
+        {
+            Console.WriteLine("Tamagotchi is alive!");
+        }
+        else
+        {
+            Console.WriteLine("Tamagotchi is dead!");
+        }
+    }
+    public void tick()
+    {
+        Hunger++;
+        Boredom++;
+        if(Boredom>10)
+        {
+           isAlive = false; 
+        }
+        if(Hunger>10)
+        {
+            isAlive = false; 
+        }
+    }
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
 }
